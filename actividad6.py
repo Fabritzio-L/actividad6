e20ede19231855e1bd76aa45dd7808dfef47ffa7
fabri <fabritzioalejandrol@gmail.com>
2025-07-24 16:58:40 -0600

    Programa final

--- actividad6.py
+++ actividad6.py
@@ -1,5 +1,3 @@
-def saludar():
-    print("Bienvenido al programa")
 def ingresar_cantidad_numeros(cantidad_numeros):
     numeros = []
     for i in range(cantidad_numeros):
@@ -19,7 +17,7 @@ def sumar_promedio_positivos_negativos(numeros):
         else:
             positivos +=1
     print(f"La suma de los numeros es de: {suma}")
-    print(f"El promedio de los numeros es: {promedio}")
+    print(f"El promedio de los numeros es: {promedio:.2f}")
     print(f"Hay {positivos} numeros positivos y {negativos} numeros negativos")
 def calcular_area_triangulo(base,altura):
     return (base * altura)/2
@@ -41,8 +39,7 @@ def mayor_menor(numeros):
             menor = i
     return f"El mayor es {mayor} y el menor es {menor}"
 while True:
-    saludar()
-    print("1. Ingresar n numeros.")
+    print("1. Operaciones con n numeros.")
     print("2. Calcular area de un triangulo")
     print("3. Verificar si un numero es par")
     print("4. Calcular promedio de calificaciones.")
@@ -56,7 +53,7 @@ while True:
             print(sumar_promedio_positivos_negativos(numeros))
         case "2":
             base = int(input("Ingrese la base del triangulo: "))
-            altura=int(input("Ingrese la altura del triangulo"))
+            altura=int(input("Ingrese la altura del triangulo: "))
             print(f"El area del triangulo es: {calcular_area_triangulo(base,altura)}")
         case "3":
             numero = int(input("Ingrese un numero: "))
@@ -67,7 +64,7 @@ while True:
         case "4":
             n= int(input("Ingrese la cantidad de calificaciones: "))
             calificaciones= ingresar_cantidad_numeros((n))
-            print(f"El promedio de las calificaciones es de: {promediar_calificaciones(calificaciones)}")
+            print(f"El promedio de las calificaciones es de: {promediar_calificaciones(calificaciones):.2f}")
             print()
         case "5":
             n= int(input("Ingrese la cantidad de numeros a ingresar: "))
